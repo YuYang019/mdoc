4d503d849bbfb26833d79d8eadb6e66a63fb29f5
maoyuyang <784406842@qq.com>
2019-05-20 23:35:37 +0800

    feat: introduce prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   root: true,
   extends: [
     'plugin:vue-libs/recommended',
-    'plugin:jest/recommended'
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   rules: {
     indent: ['error', 2, { MemberExpression: 'off' }],
-    "no-undef": ["error"],
-    'operator-linebreak': ["error", "before"]
+    'no-undef': ['error'],
+    'operator-linebreak': ['error', 'before']
   }
 }
